1f2953790e35cf0eafc185f43922d045020171d2
unknown <harinirswekrish@gmail.com>
2023-08-29 11:21:31 +0530

    Commit changes

---
+++ src/main/java/com/example/dockercicdgithub/DockerCicdGithubApplication.java
@@ -0,0 +1,21 @@
+package com.example.dockercicdgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerCicdGithubApplication {
+
+	@GetMapping("/docker")
+	public String mainMethod(){
+		return "Hello Docker CI/CD !....";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerCicdGithubApplication.class, args);
+	}
+
+}
